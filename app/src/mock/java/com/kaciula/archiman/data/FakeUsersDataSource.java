d5adfbc6188374ce32c465e10f377ee07dee57a1
Catalin Morosan <catalin.morosan@gmail.com>
2016-11-17 12:32:13 +0200

    Update mock data source with rxjava 2 and autovalue

--- app/src/mock/java/com/kaciula/archiman/data/FakeUsersDataSource.java
+++ app/src/mock/java/com/kaciula/archiman/data/FakeUsersDataSource.java
@@ -5,31 +5,19 @@ import com.kaciula.archiman.domain.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Observable;
+import io.reactivex.Observable;
 
 public class FakeUsersDataSource implements UsersDataSource {
 
     @Override
     public Observable<List<User>> getMembersOfOrganisation(String organisationName) {
         List<User> users = new ArrayList<>(6);
-        User user = new User();
-        user.username = "ka";
-        users.add(user);
-        user = new User();
-        user.username = "andrei";
-        users.add(user);
-        user = new User();
-        user.username = "arti";
-        users.add(user);
-        user = new User();
-        user.username = "marius";
-        users.add(user);
-        user = new User();
-        user.username = "stefan";
-        users.add(user);
-        user = new User();
-        user.username = "bogdan";
-        users.add(user);
+        users.add(User.create("ka"));
+        users.add(User.create("andrei"));
+        users.add(User.create("arti"));
+        users.add(User.create("marius"));
+        users.add(User.create("stefan"));
+        users.add(User.create("bogdan"));
         return Observable.just(users);
     }
 }
