d82451eee4e7d6e2bc114cd17c09364d7bc037c5
Catalin Morosan <catalin.morosan@gmail.com>
2017-05-11 09:47:37 +0300

    Clean up presenter test

--- app/src/test/java/com/kaciula/archiman/presentation/screen/home/HomePresenterTest.java
+++ app/src/test/java/com/kaciula/archiman/presentation/screen/home/HomePresenterTest.java
@@ -1,18 +1,10 @@
 package com.kaciula.archiman.presentation.screen.home;
 
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import com.kaciula.archiman.domain.entity.User;
 import com.kaciula.archiman.domain.usecase.GetUsers;
 import com.kaciula.archiman.util.scheduler.BaseSchedulerProvider;
 import com.kaciula.archiman.util.scheduler.TrampolineSchedulerProvider;
-import io.reactivex.Observable;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -29,36 +21,5 @@ public class HomePresenterTest {
 
     BaseSchedulerProvider schedulerProvider = new TrampolineSchedulerProvider();
     presenter = new HomePresenter(view, schedulerProvider, getUsers);
-  }
-
-  @Test
-  public void setupIsCalled() {
-    presenter.init();
-
-    verify(view).setup();
-  }
-
-  @Test
-  public void loadUsersCallViewToShowContent() {
-    List<User> users = new ArrayList<>();
-    users.add(User.create(1, "Bob Sacamano"));
-    when(getUsers.execute(GetUsers.RequestModel.create()))
-        .thenReturn(Observable.just(GetUsers.ResponseModel.create(users)));
-
-    presenter.start();
-
-    verify(view).showProgress();
-    verify(view).showContent();
-  }
-
-  @Test
-  public void loadUsersErrorCallViewToShowError() {
-    when(getUsers.execute(GetUsers.RequestModel.create()))
-        .thenReturn(Observable.<GetUsers.ResponseModel>error(new Exception()));
-
-    presenter.start();
-
-    verify(view).showProgress();
-    verify(view).showError();
   }
 }
