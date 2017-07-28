645504b9e884f4b92115a75585835e2a5217a76b
Catalin Morosan <catalin.morosan@gmail.com>
2017-07-28 11:50:04 +0300

    Move release files to kotlin

---
+++ app/src/release/java/com/kaciula/archiman/infrastructure/ReleaseArchimanApplication.kt
@@ -0,0 +1,31 @@
+package com.kaciula.archiman.infrastructure
+
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+import timber.log.Timber
+
+class ReleaseArchimanApplication : ArchimanApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val uncaughtExceptionHandler = ArchimanUncaughtExceptionHandler(BaseApplication.getContext())
+        Thread.setDefaultUncaughtExceptionHandler(uncaughtExceptionHandler)
+
+        if (isCrashlyticsUsed) {
+            startCrashlytics()
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+
+    override fun isCrashlyticsUsed(): Boolean {
+        // FIXME: 10/01/17 When you have set the fabric API key, replace this with true
+        return false
+    }
+
+    private fun startCrashlytics() {
+        Fabric.with(this, Crashlytics())
+        Crashlytics.setUserIdentifier(AndroidUtils.getDeviceId())
+        Crashlytics.setString("Installer", AndroidUtils.getInstaller())
+    }
+}
---
+++ app/src/release/java/com/kaciula/archiman/presentation/util/DevDrawer.kt
@@ -0,0 +1,19 @@
+package com.kaciula.archiman.presentation.util
+
+import android.app.Activity
+
+class DevDrawer(unused: Activity) {
+
+    fun onStart() {}
+
+    fun onResume() {}
+
+    fun onPause() {}
+
+    fun onStop() {}
+
+    companion object {
+
+        fun setupLogging() {}
+    }
+}
